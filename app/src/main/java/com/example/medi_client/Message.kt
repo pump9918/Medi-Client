ea12ff3683568be54462f3a840b77a019d5b0ec0
pump9918 <hoyeon9918@gmail.com>
2023-12-19 13:30:44 +0900

    [feat] #1 챗봇 메시지, 어댑터

---
+++ app/src/main/java/com/example/medi_client/Message.kt
@@ -0,0 +1,18 @@
+package com.example.medi_client
+
+class Message {
+    var message: String? = null
+    var sentBy: String? = null
+
+    constructor(message: String?, sentBy: String?) {
+        this.message = message
+        this.sentBy = sentBy
+    }
+
+    constructor() {}
+
+    companion object {
+        var SENT_BY_ME = "me"
+        var SENT_BY_BOT = "bot"
+    }
+}
---
+++ app/src/main/java/com/example/medi_client/MessageAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.medi_client
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MessageAdapter(var messageList: List<Message>) :
+    RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): ViewHolder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_chat, null)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val message = messageList[position]
+        if (message.sentBy == Message.SENT_BY_ME) {
+            holder.left_chat_view.visibility = View.GONE
+            holder.right_chat_view.visibility = View.VISIBLE
+            holder.right_chat_tv.text = message.message
+        } else {
+            holder.right_chat_view.visibility = View.GONE
+            holder.left_chat_view.visibility = View.VISIBLE
+            holder.left_chat_tv.text = message.message
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return messageList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var left_chat_view: LinearLayout
+        var right_chat_view: LinearLayout
+        var left_chat_tv: TextView
+        var right_chat_tv: TextView
+
+        init {
+            left_chat_view = itemView.findViewById<LinearLayout>(R.id.left_chat_view)
+            right_chat_view = itemView.findViewById<LinearLayout>(R.id.right_chat_view)
+            left_chat_tv = itemView.findViewById<TextView>(R.id.left_chat_tv)
+            right_chat_tv = itemView.findViewById<TextView>(R.id.right_chat_tv)
+        }
+    }
+}
