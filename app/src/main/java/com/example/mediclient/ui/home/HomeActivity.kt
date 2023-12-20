1807c2d23e4e4a5dddfae6ecd85150f4a446c335
pump9918 <hoyeon9918@gmail.com>
2023-12-20 14:28:25 +0900

    [feat] #5 네비게이션 드로어로 변경

--- app/src/main/java/com/example/mediclient/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/mediclient/ui/home/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.example.mediclient.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.example.mediclient.R
 import com.example.mediclient.databinding.ActivityHomeBinding
@@ -24,37 +26,63 @@ class HomeActivity : AppCompatActivity() {
                 .commit()
         }
 
+        // Toolbar 설정
+        setSupportActionBar(binding.toolbarHome)
+
+        // Drawer toggle 설정
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayoutHome,
+            binding.toolbarHome,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close,
+        )
+        binding.drawerLayoutHome.addDrawerListener(toggle)
+        toggle.syncState()
+
         binding.fabHomeChatbot.setOnClickListener {
             val intent = Intent(this, ChatbotActivity::class.java)
             startActivity(intent)
         }
 
-        clickBottomNavigation()
+        clickDrawNavigation()
     }
 
-    private fun clickBottomNavigation() {
-        binding.bnvHome.setOnItemSelectedListener {
-            when (it.itemId) {
+    private fun clickDrawNavigation() {
+        binding.dnvHome.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
                 R.id.menu_home -> {
                     replaceFragment(HomeFragment())
+                    binding.drawerLayoutHome.closeDrawers()
                     true
                 }
 
                 R.id.menu_chatbot -> {
                     val intent = Intent(this, ChatbotActivity::class.java)
                     startActivity(intent)
+                    binding.drawerLayoutHome.closeDrawers()
                     true
                 }
 
                 R.id.menu_mypage -> {
-                    /*추후 MypageActivity로 수정*/
-                    val intent = Intent(this, ChatbotActivity::class.java)
+                    val intent = Intent(this, ChatbotActivity::class.java) // MypageActivity로 수정
                     startActivity(intent)
+                    binding.drawerLayoutHome.closeDrawers()
                     true
                 }
 
                 else -> false
             }
+            binding.drawerLayoutHome.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayoutHome.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayoutHome.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 
